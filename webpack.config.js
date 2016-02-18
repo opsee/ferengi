4bed53f5d7dcf000d06c28020e853139a481b1fc
Sara Bee <sara.bainbridge@gmail.com>
2016-02-18 18:20:28 -0500

    Add babel loader to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,32 @@
+const path = require('path');
+
+const CONTEXT_DIR = path.join(__dirname, '/src');
+
 module.exports = {
-  source: './src/entry.js',
+  entry: './src/entry.js',
 
   output: {
     filename: 'bundle.js',
     path: 'dist',
     libraryTarget: 'umd'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$|\.jsx$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        },
+        include: [CONTEXT_DIR]
+      }
+
+    ]
+  },
+
+  resolve: {
+    extensions: ['', '.jsx', '.js', '.json', '.svg', '.png', '.jpg'],
+    modulesDirectories: ['node_modules']
   }
 };
