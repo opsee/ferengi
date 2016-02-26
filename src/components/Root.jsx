f62879bd4943e370539bb9d93906636ebc8e1a86
Sara Bee <sara.bainbridge@gmail.com>
2016-02-26 15:55:25 -0500

    Add grid.css

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -4,6 +4,10 @@ import style from './root.css';
 import Header from './Header.jsx';
 import LogoColor from 'emissary/src/js/components/global/LogoColor.jsx';
 
+/* eslint-disable no-unused-vars */
+import grid from './layout/grid.css';
+/* eslint-enable no-unused-vars */
+
 module.exports = React.createClass({
   propTypes: {
     assets: PropTypes.shape({
