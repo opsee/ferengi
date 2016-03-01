f2cffac84a08c55a86c67b8c44321977d64fc7b6
Sara Bee <sara.bainbridge@gmail.com>
2016-03-01 10:49:24 -0500

    Move server render into separate file

--- src/entry.js
+++ src/entry.js
@@ -8,41 +8,8 @@ import serialize from 'serialize-javascript';
 
 import reducer from './reducers/app';
 import routes from './routes';
+import renderServer from './modules/render-server';
 
-/**
- * We do this in order to avoid mounting the entire React app onto the body.
- */
-function renderFullPage(html, initialState = {}) {
-  // TODO: use this.props.assets for style.css/bundle.js instead of hardcoding
-  return `
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <title>Opsee</title>
-        <link rel="stylesheet" type="text/css" href="/style.css" />
-      </head>
-      <body>
-        <div id="root">${html}</div>
-        <script>
-          window.__initialState = ${initialState};
-        </script>
-        <script src="/bundle.js"></script>
-      </body>
-    </html>
-  `;
-}
-
-const getMarkup = (store) => {
-  const initialState = serialize(store.getState());
-
-  const markup = React.renderToStaticMarkup(
-    <Provider store={store} key="provider">
-      <ReduxRouter/>
-    </Provider>
-  );
-
-  return renderFullPage(markup, initialState);
-};
 
 /*
  * This rendering happens on the client-side
@@ -66,23 +33,7 @@ if (typeof document !== 'undefined') {
 
 /*
  * This is the render function used by the static site generator plugin.
+ * It will get called once per route, generating the corresponding static
+ * HTML for that route (e.g., /about will generate about.html).
  */
-module.exports = function render(locals, callback) {
-  const store = reduxReactRouter({
-    routes,
-    createHistory: createMemoryHistory
-  })(createStore)(reducer);
-
-  store.dispatch(match(locals.path, (error, redirectLocation, routerState) => {
-    if (error) {
-      console.error('AN ERROR OCCURRED!', error);
-      callback(error);
-    } else if (redirectLocation) {
-      console.log('TODO: redirect');
-    } else if (!routerState) {
-      console.log('404 not found');
-    } else {
-      callback(null, getMarkup(store));
-    }
-  }));
-};
+module.exports = (locals, callback) => renderServer(locals, callback);
---
+++ src/modules/render-server.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { createStore, compose } from 'redux';
+import { Provider } from 'react-redux';
+import { createMemoryHistory, createBrowserHistory } from 'history';
+import { ReduxRouter } from 'redux-router';
+import { reduxReactRouter, match } from 'redux-router/server';
+import serialize from 'serialize-javascript';
+
+import reducer from '../reducers/app';
+import routes from '../routes';
+
+/**
+ * We do this in order to avoid mounting the entire React app onto the body.
+ */
+function renderFullPage(html, initialState = {}) {
+  // TODO: use this.props.assets for style.css/bundle.js instead of hardcoding
+  return `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Opsee</title>
+        <link rel="stylesheet" type="text/css" href="/style.css" />
+      </head>
+      <body>
+        <div id="root">${html}</div>
+        <script>
+          window.__initialState = ${initialState};
+        </script>
+        <script src="/bundle.js"></script>
+      </body>
+    </html>
+  `;
+}
+
+const getMarkup = (store) => {
+  const initialState = serialize(store.getState());
+
+  const markup = React.renderToStaticMarkup(
+    <Provider store={store} key="provider">
+      <ReduxRouter/>
+    </Provider>
+  );
+
+  return renderFullPage(markup, initialState);
+};
+
+module.exports = function renderServer(locals, callback) {
+  const store = reduxReactRouter({
+    routes,
+    createHistory: createMemoryHistory
+  })(createStore)(reducer);
+
+  store.dispatch(match(locals.path, (error, redirectLocation, routerState) => {
+    if (error) {
+      console.error('AN ERROR OCCURRED!', error);
+      callback(error);
+    } else if (redirectLocation) {
+      console.log('TODO: redirect');
+    } else if (!routerState) {
+      console.log('404 not found');
+    } else {
+      callback(null, getMarkup(store));
+    }
+  }));
+};
