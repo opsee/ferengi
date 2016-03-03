fb49d3f3f389fd637ba32b3b179092e2d39d7fff
Sara Bee <sara.bainbridge@gmail.com>
2016-03-03 11:53:09 -0500

    More tweakz

--- src/components/panels/HeroPanel.jsx
+++ src/components/panels/HeroPanel.jsx
@@ -9,15 +9,16 @@ export default React.createClass({
     return (
       <Panel>
         <div className={style.heroPanel}>
-          <div>
-            <h1 className={style.header}>
-              Health checks have you covered
-            </h1>
-            <p className={style.subHeader}>
-              Whether you're watching 1 instance or 1000, Opsee lets you be sure
-              that your services are always responding the way you expect.
-            </p>
+          <div className={style.heroContent}>
+            <div className={style.heading}>
+              <h1>Health checks have you covered</h1>
+              <p className={style.subHeader}>
+                Whether you're watching 1 instance or 1000, Opsee lets you be sure
+                that your services are always responding the way you expect.
+              </p>
+            </div>
 
+            <h2>Try it for yourself. Run a check:</h2>
             <div className={style.widgetWrap}>
               <TryCheck />
             </div>
