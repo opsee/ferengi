aceb00f61ca3b4d08473b4220f4de5845ccaba25
Sara Bee <sara.bainbridge@gmail.com>
2016-03-03 11:12:35 -0500

    Add TryCheck widget

--- src/components/panels/TryCheckPanel.jsx
+++ src/components/panels/TryCheckPanel.jsx
@@ -1,13 +1,18 @@
 import React, { PropTypes } from 'react';
-import Panel from './Panel.jsx';
 import style from './tryCheckPanel.css';
+
+import Panel from './Panel';
+import TryCheck from '../widgets/TryCheck';
+
 
 export default React.createClass({
   render() {
     return (
       <Panel>
         <div className={style.tryCheckPanel}>
-          try check panel
+          <div className={style.widgetWrap}>
+            <TryCheck />
+          </div>
         </div>
       </Panel>
     );
---
+++ src/components/widgets/TryCheck.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './tryCheck.css';
+
+export default React.createClass({
+  render() {
+    return (
+      <div>
+        <input className={style.input} type="text" placeholder="https://pepe.therarest.com:8420" />
+      </div>
+    );
+  }
+});
