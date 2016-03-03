f46c7ce7df107585c61f3071b0969809b01b274d
Sara Bee <sara.bainbridge@gmail.com>
2016-03-03 15:38:26 -0500

    Add promise middleware

--- src/actions/checks.js
+++ src/actions/checks.js
@@ -1,14 +1,15 @@
-import { createAction } from 'redux-actions';
 import { CHECK_URL } from './constants';
 
 export function checkURL(url) {
   return dispatch => {
     dispatch({
       type: CHECK_URL,
-      payload: new Promise((resolve, reject) => {
-        console.log('done!');
-        resolve({ url });
-      })
+      payload: {
+        promise: new Promise((resolve, reject) => {
+          console.log('done!');
+          resolve({ url });
+        })
+      }
     });
   };
 }
--- src/modules/render-client.js
+++ src/modules/render-client.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import { createStore, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { applyMiddleware, createStore, compose } from 'redux';
 import { Provider } from 'react-redux';
 import { createHistory } from 'history';
 import { ReduxRouter, reduxReactRouter } from 'redux-router';
+import { promiseMiddleware } from 'emissary/src/js/modules/promiseMiddleware';
 
 import reducer from '../reducers/app';
 import routes from '../routes';
 
 export default () => {
   const initialState = window.__initialState;
+
   const store = compose(
+    applyMiddleware( thunk, promiseMiddleware ),
     reduxReactRouter({ createHistory })
   )(createStore)(reducer, initialState);
 
