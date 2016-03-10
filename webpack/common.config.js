a082a8ecfcefd65793c24298f27ecad5fca39cb2
Sara Bee <sara.bainbridge@gmail.com>
2016-03-10 12:48:56 -0500

    Add display font

--- webpack/common.config.js
+++ webpack/common.config.js
@@ -64,8 +64,11 @@ module.exports = {
         test: /\.(png|jpg|svg)$/,
         loader: 'url-loader?limit=8192',
         include: INCLUDE_DIRS
+      }, {
+        test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        loader: 'file-loader',
+        include: INCLUDE_DIRS
       }
-
     ]
   },
 
