98dedc47ebd1974ae50d937a093750a16f407f45
Sara Bee <sara.bainbridge@gmail.com>
2016-03-14 19:57:34 -0400

    dead code :skull:

--- src/components/forms/Button.jsx
+++ src/components/forms/Button.jsx
@@ -98,32 +98,3 @@ const Button = React.createClass({
 });
 
 export default Button;
-// =======
-// import React, { PropTypes } from 'react';
-// import style from './button.css';
-
-// export default React.createClass({
-//   propTypes: {
-//     children: PropTypes.node,
-//     className: PropTypes.string
-//   },
-
-//   getDefaultProps() {
-//     return {
-//       className: '',
-//     };
-//   },
-
-//   getClass() {
-//     return [this.props.className, style.button].join(' ');
-//   },
-
-//   render() {
-//     return (
-//       <button className={this.getClass()}>
-//         {this.props.children}
-//       </button>
-//     );
-//   }
-// });
-// >>>>>>> master
