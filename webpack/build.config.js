5b971cdb991be91d710ae899d17ea454ecc2394d
Corey Light <coreylight@gmail.com>
2016-03-16 14:27:00 -0500

    meta info and sitemap

--- webpack/build.config.js
+++ webpack/build.config.js
@@ -3,6 +3,7 @@ const baseConfig = require('./common.config.js');
 const merge = require('webpack-merge');
 const compression = require('compression-webpack-plugin');
 const StaticSiteGeneratorPlugin = require('static-site-generator-webpack-plugin');
+const sitemap =  require('sitemap-webpack-plugin');
 
 const PATHS = [
   '/', '/about', '/how', '/features'
@@ -11,6 +12,7 @@ const PATHS = [
 module.exports = merge(baseConfig, {
   plugins: [
     new StaticSiteGeneratorPlugin('bundle.js', PATHS, {}),
+    new sitemap('https://opsee.com', PATHS, 'sitemap.xml'),
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify(process.env.NODE_ENV)
