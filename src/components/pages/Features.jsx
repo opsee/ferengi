20a798d65beede1b034868a543cfe27795ee8d1d
Sara Bee <sara.bainbridge@gmail.com>
2016-03-16 17:45:58 -0700

    New copy for features page

--- src/components/pages/Features.jsx
+++ src/components/pages/Features.jsx
@@ -8,6 +8,7 @@ import Padding from '../layout/Padding';
 import SkewDivider from '../layout/SkewDivider';
 import SkewPanel from '../panels/SkewPanel';
 import SignUpPanel from '../panels/SignUpPanel'
+import Button from '../forms/Button';
 
 import BaseSVG from '../images/BaseSVG';
 import actionsSVG from '../images/home-actions.svg';
@@ -15,6 +16,8 @@ import bastionSVG from '../images/home-bastion3.svg';
 import checksSVG from '../images/home-checks2.svg';
 import checkDiagramSVG from '../images/product-checks.svg';
 import notificationsSVG from '../images/product-notifications.svg';
+import installStepsSVG from '../images/install-steps2.svg';
+import howBastionSVG from '../images/how-bastion.svg';
 
 export default React.createClass({
   render() {
@@ -33,18 +36,48 @@ export default React.createClass({
                 <Padding tb={1}>
                   <h2>Deep integration with <span className='text-accent'>AWS</span></h2>
                 </Padding>
-                  <h3>Health checks update automatically when your environment changes.</h3>
 
-                <p className="prose">
-                  You never need to maintain Opsee health checks. When you create a
-                  check for a dynamic target like a Security Group, Elastic Load
-                  Balancer, or Auto Scale Group, we use AWS APIs to scan your
-                  environment and update your checks automatically.
-                </p>
+                <ul>
+                  <li><span className="prose">Just add our instance to your AWS environment</span></li>
+                  <li><span className="prose">We automatically and continuously discover your environment</span></li>
+                  <li><span className="prose">No configurations to manage</span></li>
+                </ul>
+
+                <Padding tb={2}>
+                  <Button className={style.button} secondary chevron>How it works</Button>
+                </Padding>
               </SplitColumn>
 
               <SplitColumn order={1}>
-                <BaseSVG svg={checkDiagramSVG} style={{maxWidth: '100%'}} />
+                <BaseSVG svg={checkDiagramSVG} />
+              </SplitColumn>
+            </SplitPanel>
+          </div>
+
+          <SkewDivider className={style.offsetDivider} />
+
+          <div className={style.section}>
+            <SplitPanel>
+              <SplitColumn order={1}>
+                <BaseSVG svg={installStepsSVG} />
+              </SplitColumn>
+
+              <SplitColumn order={2}>
+                <Padding tb={1}>
+                  <h2><span className='text-accent'>Zero</span> maintenance</h2>
+                </Padding>
+
+                <ul>
+                  <li><span className="prose">Easy install with just AWS keys</span></li>
+                  <li><span className="prose">Your health checks dynamically adjust to your environment</span></li>
+                  <li><span className="prose">Clean uninstall via Cloudformation</span></li>
+                </ul>
+
+                <Padding tb={2}>
+                  <Button className={style.button} secondary chevron>
+                    Learn more about our AWS instance
+                  </Button>
+                </Padding>
               </SplitColumn>
             </SplitPanel>
           </div>
@@ -60,18 +93,20 @@ export default React.createClass({
 
                 <SplitColumn order={2}>
                   <Padding tb={1}>
-                    <h2><span className='text-accent'>Rich notifications</span> help you solve problems faster</h2>
+                    <h2>Rich, actionable <span className='text-accent'>notifications</span></h2>
                   </Padding>
 
-                  <h3>Health checks update automatically when your environment changes.</h3>
+                  <ul>
+                    <li><span className="prose">See failing responses in context</span></li>
+                    <li><span className="prose">Get notified anywhere: email, Slack, webhooks, or PagerDuty</span></li>
+                    <li><span className="prose">Take action on problem instances (restart, stop, or terminate)</span></li>
+                  </ul>
 
-                  <p className="prose">
-                    We do more with notifications. Opsee can send
-                    notifications to email, Slack, and webhooks with ease. We show
-                    you failing responses in notifications, and if the failure
-                    happened in a group, we tell you where the problem occurred.
-                    We even have tools to take action on problem instances in the Opsee app.
-                  </p>
+                  <Padding tb={2}>
+                    <Button className={style.button} secondary chevron>
+                      Learn more about notifcations
+                    </Button>
+                  </Padding>
                 </SplitColumn>
               </SplitPanel>
             </Padding>
@@ -84,17 +119,18 @@ export default React.createClass({
               <SplitPanel>
                 <SplitColumn order={2}>
                   <Padding tb={1}>
-                    <h2><span className='text-accent'>Assertions:</span> more than status codes</h2>
+                    <h2><span className='text-accent'>Assertions:</span> more just than status codes</h2>
                   </Padding>
 
-                  <h3>Be sure your services are responding as expected.</h3>
+                  <ul>
+                    <li><span className="prose">Parse headers, JSON repsonse bodies, and status codes easily</span></li>
+                  </ul>
 
-                  <p className="prose">
-                    Your services can return a 200 status code even if they’re
-                    broken or misconfigured. Assertions let you dig deep into the
-                    response and ensure that everything is working exactly as
-                    expected, from headers to JSON keys in the response body.
-                  </p>
+                  <Padding tb={2}>
+                    <Button className={style.button} secondary chevron>
+                      Learn more about notifcations
+                    </Button>
+                  </Padding>
                 </SplitColumn>
 
                 <SplitColumn order={1}>
@@ -114,16 +150,14 @@ export default React.createClass({
 
               <SplitColumn order={2}>
                 <Padding tb={1}>
-                  <h2><span className='text-accent'>Complete coverage</span> with just AWS keys</h2>
+                  <h2>Clean & <span className='text-accent'>simple</span></h2>
                 </Padding>
 
-                <h3>Deep integration with your environment, zero effort.</h3>
-
-                <p className="prose">
-                  We install with just AWS keys — no SSH or cURL required. Use our
-                  IAM guide to add Opsee’s instance to your environment and we do
-                  the rest. We even try to create health checks for you.
-                </p>
+                <ul>
+                  <li><span className="prose">Failing health checks tell you exactly what’s wrong</span></li>
+                  <li><span className="prose">Find problems without staring at graphs</span></li>
+                  <li><span className="prose">Responsive UI works on any device</span></li>
+                </ul>
               </SplitColumn>
             </SplitPanel>
           </div>
@@ -134,19 +168,24 @@ export default React.createClass({
             <SplitPanel>
               <SplitColumn order={2}>
                 <Padding tb={1}>
-                  <h2>Opsee is monitoring for <span className='text-accent'>on-call dev teams</span></h2>
+                  <h2><span className='text-accent'>Security</span>-minded</h2>
                 </Padding>
 
-                <h3>Effortless integration and rich notifications so you can get back to coding.</h3>
+                <ul>
+                  <li><span className="prose">Our CloudFormation templates and IAM roles give you total transparency</span></li>
+                  <li><span className="prose">No third party software on your systems</span></li>
+                  <li><span className="prose">Secure communication between Opsee & our instance</span></li>
+                </ul>
 
-                <p className="prose">
-                  Opsee integrates with your AWS environment and creates health
-                  checks for you, so you can get back to coding.
-                </p>
+                <Padding tb={2}>
+                  <Button className={style.button} secondary chevron>
+                    Learn more about our IAM & CloudFormation
+                  </Button>
+                </Padding>
               </SplitColumn>
 
               <SplitColumn order={1}>
-                <BaseSVG svg={actionsSVG} style={{maxWidth: '100%'}} />
+                <BaseSVG svg={howBastionSVG} style={{maxWidth: '100%'}} />
               </SplitColumn>
             </SplitPanel>
           </div>
