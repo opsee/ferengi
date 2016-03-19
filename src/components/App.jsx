6eae8100e297878dfe44457f99d2929147c8f9b4
Corey Light <coreylight@gmail.com>
2016-03-19 11:18:28 -0500

    do teh signup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,16 +3,22 @@ import { connect } from 'react-redux';
 
 import {yeller} from '../modules';
 import Footer from './Footer.jsx';
-import Header from './Header.jsx';
 import style from './app.css';
 import Analytics from './global/Analytics';
 
 const App = React.createClass({
   propTypes: {
-    children: PropTypes.node
+    children: PropTypes.node,
+    router: PropTypes.shape({
+      location: PropTypes.object
+    })
   },
   componentWillMount(){
     yeller.configure();
+    const ref = this.props.router.location.query.referrer;
+    if (ref && typeof window !== 'undefined'){
+      window.localStorage.setItem('referrer', ref);
+    }
   },
   render() {
     return (
@@ -33,6 +39,6 @@ const App = React.createClass({
 export default connect(
   // Use a selector to describe state
   state => ({
-    routerState: state.router
+    router: state.router
   })
 )(App);
--- src/components/panels/SignUpPanel.jsx
+++ src/components/panels/SignUpPanel.jsx
@@ -1,40 +1,119 @@
+import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import _ from 'lodash';
+
 import Panel from './Panel.jsx';
-import React from 'react';
 import Input from '../forms/Input';
 import Button from '../forms/Button';
 import Padding from '../layout/Padding';
-
+import * as actions from '../../actions/user';
 import style from './signUpPanel.css';
 
-export default React.createClass({
+const SignUpPanel = React.createClass({
+  propTypes: {
+    location: PropTypes.object,
+    actions: PropTypes.shape({
+      signup: PropTypes.func.isRequired
+    }),
+  },
+  getInitialState() {
+    return {
+      email: undefined,
+      referrer: this.getReferrer(),
+      name: 'default',
+      tos: false
+    };
+  },
+  getReferrer(){
+    let ref = this.props.location.query.referrer;
+    if (!ref && typeof window !== 'undefined'){
+      ref = window.localStorage.getItem('referrer');
+    }
+    return ref;
+  },
+  getStatus(){
+    return this.props.redux.asyncActions.signup.status;
+  },
+  handleInputChange(e, data){
+    if (e && e.target){
+      this.setState({
+        [e.target.name]: e.target.value
+      });
+    }
+  },
+  handleSubmit(e){
+    e.preventDefault();
+    this.props.actions.signup(this.state);
+  },
+  renderAlert(){
+    if (this.getStatus() && this.getStatus() !== 'pending'){
+      let msg = 'Something went wrong.';
+      if (typeof this.getStatus() === 'object'){
+        msg = _.get(this.getStatus(), 'message') || msg;
+      }
+      return (
+        <Padding t={1} b={1}>
+          {msg}
+        </Padding>
+      );
+    }
+    return null;
+  },
+  renderForm(){
+    if (this.getStatus() === 'success'){
+      return (
+        <Padding t={2} b={1}>
+          We&rsquo;ve got your info! Check your email to get started.
+        </Padding>
+      )
+    }
+    return (
+      <div>
+        <Padding t={2} b={1}>
+          <Input className={style.input} name="email" placeholder="Your email" value={this.state.email} type="email" onChange={this.handleInputChange}/>
+          <div>
+            <a href="/beta-tos" target="_blank">Terms of Service</a><br/>
+            <label>I accept the Terms of Service referenced above
+              <input name="tos" value={this.state.tos} type="checkbox" onChange={this.handleInputChange} required/>
+            </label>
+          </div>
+        </Padding>
+        {this.renderAlert()}
+        <Padding t={1} b={2}>
+          <Button className={style.button} type="submit" disabled={this.getStatus() === 'pending'}>
+            {this.getStatus() === 'pending' ? 'Submitting...' : 'Sign up for Opsee'}
+          </Button>
+        </Padding>
+      </div>
+    )
+  },
   render() {
     return (
       <Panel>
         <div className={style.panel}>
           <div className={style.container}>
-
             <div className="text-center">
               <h1 className={style.heading}>Ready to <span className="text-accent">get Opsee?</span></h1>
-              <div className={['prose-darkbg', style.prose].join(' ')}>Opsee is free during public beta. Join now by giving us your email address below, and we'll send you a link to your very own Opsee account.</div>
-           </div>
-
-            <div>
-              <div>
-                <Padding t={2} b={1}>
-                  <Input className={style.input} placeholder="Your name"/>
-                  <Input className={style.input} placeholder="Your email"/>
-                </Padding>
-              </div>
-
-              <div>
-                <Padding t={1} b={2}>
-                  <Button className={style.button}>Sign up for Opsee</Button>
-                </Padding>
-              </div>
-            </div>
+              <div className={['prose-darkbg', style.prose].join(' ')}>Opsee is free during public beta. Join now by giving us your email address below, and we&rsquo;ll send you a link to your very own Opsee account.</div>
+             </div>
+            <form onSubmit={this.handleSubmit}>
+              {this.renderForm()}
+            </form>
           </div>
         </div>
       </Panel>
     );
   }
 });
+
+const mapStateToProps = (state) => ({
+  redux: state,
+  location: state.router.location
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  actions: bindActionCreators(actions, dispatch)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignUpPanel);
