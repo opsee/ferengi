23c35b4f86b3461a306aca73f8c8a82c57527cd9
Sara Bee <sara.bainbridge@gmail.com>
2016-03-19 11:46:58 -0700

    ?????

--- src/components/about/Hiring.jsx
+++ src/components/about/Hiring.jsx
@@ -6,7 +6,7 @@ export default React.createClass({
     return (
       <div>
         <h2 className="text-accent">We&rsquo;re Hiring!</h2>
-      <p>We’re always looking for talented people to join, so even if you don’t see a position listed, reach out to us. Just send an email with your résumé and a little about yourself to <a href="mailto:work@opsee.co?subject=love too work at opsee">work@opsee.co</a>. Every thoughtful application we receive will be replied to, we promise.</p>
+        <p>We’re always looking for talented people to join, so even if you don’t see a position listed, reach out to us. Just send an email with your résumé and a little about yourself to <a href="mailto:work@opsee.co?subject=love too work at opsee">work@opsee.co</a>. Every thoughtful application we receive will be replied to, we promise.</p>
 
         <Padding t={1}>
           <Expandable noFade>
@@ -17,18 +17,18 @@ export default React.createClass({
             <p>To apply, send an email with your résumé and a little about yourself to <a href="mailto:work@opsee.co">work@opsee.co</a>.</p>
             <h3>Requirements</h3>
             <ul className="bullet_list">
-              <li>At least three years of experience working with JavaScript professionally, and a strong portfolio of launched projects</li>
-              <li>Experience with modern Javascript frameworks like React.js (which we use) and mastery of component/directive design</li>
-              <li>Expertise at testing and performance-tuning Javascript in the browser</li>
-              <li>Experience with responsive design and mobile-first design principles</li>
+              <li><p>At least three years of experience working with JavaScript professionally, and a strong portfolio of launched projects</p></li>
+              <li><p>Experience with modern Javascript frameworks like React.js (which we use) and mastery of component/directive design</p></li>
+              <li><p>Expertise at testing and performance-tuning Javascript in the browser</p></li>
+              <li><p>Experience with responsive design and mobile-first design principles</p></li>
             </ul>
             <h3>Nice to haves</h3>
             <ul className="bullet_list">
-              <li>Previous work in small startup environments</li>
-              <li>Experience with modern browser technologies like WebSockets</li>
-              <li>Background in computer science</li>
-              <li>Previous work on marketing websites, and with marketing departments</li>
-              <li>Mobile application development experience</li>
+              <li><p>Previous work in small startup environments</p></li>
+              <li><p>Experience with modern browser technologies like WebSockets</p></li>
+              <li><p>Background in computer science</p></li>
+              <li><p>Previous work on marketing websites, and with marketing departments</p></li>
+              <li><p>Mobile application development experience</p></li>
             </ul>
             </div>
           </Expandable>
@@ -40,33 +40,33 @@ export default React.createClass({
               <p>Opsee is looking for talented application developers to help craft our APIs, and design and build new product features. Our language of choice is Go, so expertise is a plus, but it is required. Professional experience developing web applications and a willingness to learn are the only requirements.</p>
               <p>We’re a small team, and your talents will be put to wide use. This can include:</p>
               <ul className="bullet_list">
-                <li>Designing, building and deploying Go microservices</li>
-                <li>Adding functionality to our streaming event processor</li>
-                <li>Sketching the design of a new feature with the rest of the team</li>
-                <li>Helping to scale our build, test, and deployment tooling, and our infrastructure</li>
+                <li><p>Designing, building and deploying Go microservices</p></li>
+                <li><p>Adding functionality to our streaming event processor</p></li>
+                <li><p>Sketching the design of a new feature with the rest of the team</p></li>
+                <li><p>Helping to scale our build, test, and deployment tooling, and our infrastructure</p></li>
               </ul>
               <p>To apply, send an email with your résumé and a little about yourself to <a href="mailto:work@opsee.co">work@opsee.co</a>.</p>
               <h3>Requirements</h3>
               <ul className="bullet_list">
-                <li>It’s important to us that you’ve been building software professionally for a few years, and have a portfolio of launched work that we can talk about</li>
-                <li>Having the competence and confidence to solve a variety of development tasks with strong, maintainable code is critical</li>
-                <li>As is being able to work and communicate well with our team</li>
+                <li><p>It’s important to us that you’ve been building software professionally for a few years, and have a portfolio of launched work that we can talk about</p></li>
+                <li><p>Having the competence and confidence to solve a variety of development tasks with strong, maintainable code is critical</p></li>
+                <li><p>As is being able to work and communicate well with our team</p></li>
               </ul>
               <h3>Nice to haves</h3>
               <ul className="bullet_list">
-                <li>We make and advocate for distributed systems, and we’re building a product to monitor them. It’s in our blood. If that interests you, you could fit in well here</li>
-                <li>Previous work on small startup teams will help you feel at home in the sometimes-chaotic environment</li>
-                <li>A strong understanding of AWS services and infrastructure will give you some immediate clarity into what we’re building</li>
-                <li>Previous experience with Go</li>
+                <li><p>We make and advocate for distributed systems, and we’re building a product to monitor them. It’s in our blood. If that interests you, you could fit in well here</p></li>
+                <li><p>Previous work on small startup teams will help you feel at home in the sometimes-chaotic environment</p></li>
+                <li><p>A strong understanding of AWS services and infrastructure will give you some immediate clarity into what we’re building</p></li>
+                <li><p>Previous experience with Go</p></li>
               </ul>
             </div>
           </Expandable>
         </Padding>
         <h3>Benefits</h3>
         <ul className="bullet_list">
-          <li>Compensation is competitive and includes a generous equity grant and full medical, dental, and vision benefits</li>
-          <li>Unlimited budget for your workstation</li>
-          <li>Paid time off</li>
+          <li><p>Compensation is competitive and includes a generous equity grant and full medical, dental, and vision benefits</p></li>
+          <li><p>Unlimited budget for your workstation</p></li>
+          <li><p>Paid time off</p></li>
         </ul>
         <h3>About Opsee</h3>
         <p>We&rsquo;re located in the beautiful <a href="http://heavybit.com/clubhouse" target="_blank">Heavybit clubhouse</a> in San Francisco, CA where we offer free catered lunches, coffee, tea, and other beverages. We also offer, and actively support, remote working.</p>
--- src/components/about/PressKit.jsx
+++ src/components/about/PressKit.jsx
@@ -7,26 +7,30 @@ export default React.createClass({
         <h2 className="text-accent">Press Kit</h2>
         <p>A collection of Opsee brand resources and assets for your use:</p>
 
-        <h3>Logo Usage Guidelines</h3>
-        <ul>
-          <li>Do not modify these logos, or use them suggest sponsorship or endorsement by Opsee.</li>
-          <li>Do not use these logos to confuse any other brand with Opsee</li>
-          <li>Do not use these logos on colors that clash, or are difficult to read. Different versions of the logo are provided for light and dark backgrounds - please use them appropriately. Monochrome logos are also provided if your usage requires them, or if you&rsquo;re in doubt about which color version will work best.</li>
-          <li>Do not change the colors, rotate, or obstruct any part of the logo</li>
-        </ul>
+        <div className='section'>
+          <h3>Logo Usage Guidelines</h3>
+          <ul>
+            <li><p>Do not modify these logos, or use them suggest sponsorship or endorsement by Opsee.</p></li>
+            <li><p>Do not use these logos to confuse any other brand with Opsee</p></li>
+            <li><p>Do not use these logos on colors that clash, or are difficult to read. Different versions of the logo are provided for light and dark backgrounds - please use them appropriately. Monochrome logos are also provided if your usage requires them, or if you&rsquo;re in doubt about which color version will work best.</p></li>
+            <li><p>Do not change the colors, rotate, or obstruct any part of the logo</p></li>
+          </ul>
+        </div>
 
-        <h3>Logo Pack</h3>
+        <div className='section'>
+          <h3>Logo pack</h3>
 
-        <p>All logo variants, available for download as a .ZIP file:</p>
+          <p>All logo variants, available for download as a .ZIP file:</p>
 
-        <div className="row padding-tb">
-          <div className="col-xs-12 text-center">
-            <div className="padding-b">
-              <a href="https://s3-us-west-1.amazonaws.com/opsee-public-images/opseelogos-all.zip"><img src={'/' + require('../images/press/opseelogos-all.png')} alt="all Opsee logos for download"/></a>
-            </div>
-            <div className="padding-b">
-              <a className="btn btn-primary btn-block" href="https://s3-us-west-1.amazonaws.com/opsee-public-images/opseelogos-all.zip">DOWNLOAD</a>
-              <small>All logos come in small and large PNG, and vector SVG formats (161kB .ZIP file)</small>
+          <div className="row padding-tb">
+            <div className="col-xs-12 text-center">
+              <div className="padding-b">
+                <a href="https://s3-us-west-1.amazonaws.com/opsee-public-images/opseelogos-all.zip"><img src={'/' + require('../images/press/opseelogos-all.png')} alt="all Opsee logos for download"/></a>
+              </div>
+              <div className="padding-b">
+                <a className="btn btn-primary btn-block" href="https://s3-us-west-1.amazonaws.com/opsee-public-images/opseelogos-all.zip">DOWNLOAD</a>
+                <small>All logos come in small and large PNG, and vector SVG formats (161kB .ZIP file)</small>
+              </div>
             </div>
           </div>
         </div>
