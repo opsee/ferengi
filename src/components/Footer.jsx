365ffc86cd637c490df3ae326d3cfb9c90f20763
Sara Bee <sara.bainbridge@gmail.com>
2016-03-20 17:31:51 -0400

    Nits :bug:

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import style from './footer.css';
 
+import BaseSVG from './images/BaseSVG';
+import logoSVG from './images/logos/opseelogo-screen-dark-one.svg';
+
 export default React.createClass({
   render() {
     return (
       <div className={style.footer}>
-        <div className="text-center">
-          <p>Made with &hearts; by Opsee Co.</p>
-          <p>123 9th Street &bull; San Francisco, CA</p>
+        <div className={style.logoGroup}>
+          <BaseSVG className={style.logo} svg={logoSVG} />
+
+          <p className={style.prose}>Made with &hearts; by Opsee Co.</p>
+          <p className={style.prose}>123 9th Street &bull; San Francisco, CA</p>
         </div>
       </div>
     );
