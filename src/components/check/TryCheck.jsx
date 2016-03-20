4edbe4ef37c84b3731930913653d3eed8170a06d
Sara Bee <sara.bainbridge@gmail.com>
2016-03-20 18:33:38 -0400

    linting

--- src/components/check/TryCheck.jsx
+++ src/components/check/TryCheck.jsx
@@ -85,7 +85,7 @@ const TryCheck = React.createClass({
           </div>
 
           <div className={style.prose}>
-            <h2>Health checks are <span className='text-accent'>more</span> than just a status code</h2>
+            <h2>Health checks are <span className="text-accent">more</span> than just a status code</h2>
             <p>Lots of webservers will happily return a status code of 200 even if the underlying service is broken or misconfigured. Assertions let you dig deep into the health check response to ensure that everything is working exactly how you expect. Pull out headers and parse some JSON. Go on, it's fun.</p>
           </div>
 
