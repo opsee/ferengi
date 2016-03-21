d53572ade4a6a1b782e123783c992c165939adab
Sara Bee <sara.bainbridge@gmail.com>
2016-03-21 12:53:35 -0400

    Fix heavybutt address

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,7 +16,7 @@ export default React.createClass({
 
         <div className={style.logoGroup}>
           <p className={style.prose}>Made with &hearts; by Opsee Co.</p>
-          <p className={style.prose}>123 9th Street &bull; San Francisco, CA</p>
+          <p className={style.prose}>325 9th St &bull; San Francisco, CA</p>
           <p className={style.prose}><Link to="https://twitter.com/GetOpsee">Follow @opseeco</Link></p>
         </div>
       </div>
