9715a1b9768da8f52f114c76375d68451cd09a7a
Corey Light <coreylight@gmail.com>
2016-03-20 21:42:12 -0500

    formdataaaaaaaaa

--- webpack/build.config.js
+++ webpack/build.config.js
@@ -15,8 +15,7 @@ var plugins = [
   new webpack.DefinePlugin({
     'process.env': {
       NODE_ENV: JSON.stringify(process.env.NODE_ENV)
-    },
-    'FormData': JSON.stringify(null)
+    }
   }),
 ]
 
