f8495aa80c0c9c4cfdc549161173336c4ce8ae5b
Sara Bee <sara.bainbridge@gmail.com>
2016-03-28 16:45:25 -0400

    Add new SVGs

--- src/components/pages/Index.jsx
+++ src/components/pages/Index.jsx
@@ -6,6 +6,11 @@ import HeroPanel from '../panels/HeroPanel';
 import SignUpPanel from '../panels/SignUpPanel';
 import SummaryPanels from '../panels/SummaryPanels';
 
+import BaseSVG from '../images/BaseSVG';
+import illustAWSKey from '../images/illust_aws_key-01.svg';
+import illustBrowser from '../images/illust_browsers-01.svg';
+import illustChecks from '../images/illust_checks-01.svg';
+
 /*eslint-disable no-unused-vars*/
 import style from './index.css';
 import styleConstants from '../../constants/styleConstants';
@@ -15,11 +20,29 @@ export default React.createClass({
   render() {
     return (
       <div>
-        <Header theme="dark"/>
+        <SkewPanel backgroundColor="#303030" skewTop={false}>
+          <Header theme="light"/>
 
-        <div>
           <HeroPanel />
+        </SkewPanel>
+
+        <div className={style.grid}>
+          <div className={style.col}>
+            <BaseSVG svg={illustAWSKey} />
+            <div>
+              step 01
+            </div>
+          </div>
+
+          <div className={style.col}>
+            <BaseSVG svg={illustBrowser} />
+          </div>
+
+          <div className={style.col}>
+            <BaseSVG svg={illustChecks} />
+          </div>
         </div>
+
 
         <div>
           <SummaryPanels />
