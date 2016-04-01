41694028d55405fec530808db78ee0a039f47673
Sara Bee <sara.bainbridge@gmail.com>
2016-04-01 09:56:06 -0400

    Mobile stylin

--- src/components/pages/Integrations.jsx
+++ src/components/pages/Integrations.jsx
@@ -32,64 +32,66 @@ export default React.createClass({
 
         <Panel>
           <div className="row">
-            <div className="col center-xs col-xs-12 col-sm-3 first-xs">
-              <Padding a={2}>
+            <div className="col center-xs col-xs-12 col-sm-4 col-md-3 first-xs">
+              <div className={style.logo}>
                 <BaseSVG svg={awsSVG} />
-              </Padding>
+              </div>
             </div>
 
-            <div className="col col-xs-12 col-sm-9 last-xs">
-              <div>
-                <h2 id="ec2">Opsee + AWS = <span className="text-accent">&hearts;</span></h2>
-                <a href="#ec2"><small>(link icon)</small></a>
+            <div className="col col-xs-12 col-sm-8 col-md-9 last-xs">
+              <div className={style.prosePanel}>
+                <div className="center-xs">
+                  <h2 id="ec2">Opsee + <span className="text-accent">AWS</span> = &hearts;</h2>
+                  <a href="#ec2"><small>(link icon)</small></a>
+                </div>
+
+                <p>Amazon EC2 Container Service (ECS) is a container management service
+                that lets you run applications from Docker containers on clusters of EC2 instances.</p>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In aliquam malesuada magna, finibus sollicitudin metus porta ac.</p>
+
+
+                <Padding tb={2} r={4}>
+                  <Padding tb={2}>
+                  <div className="row">
+                    <div className="col col-xs-2 text-center">
+                      <BaseSVG className={style.awsServiceSVG} svg={ecsSVG} />
+                    </div>
+
+                    <div className="col col-xs-10">
+                      <h3>ECS</h3>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In aliquam malesuada magna, finibus sollicitudin metus porta ac.</p>
+                    </div>
+                  </div>
+                  </Padding>
+
+                  <Padding tb={2}>
+                   <div className="row">
+                    <div className="col col-xs-2 text-center">
+                      <BaseSVG className={style.awsServiceSVG} svg={rdsSVG} />
+                    </div>
+
+                    <div className="col col-xs-10">
+                      <h3>RDS</h3>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In aliquam malesuada magna, finibus sollicitudin metus porta ac.</p>
+                    </div>
+                  </div>
+                  </Padding>
+
+                  <Padding tb={2}>
+                   <div className="row">
+                    <div className="col col-xs-2 text-center">
+                      <BaseSVG className={style.awsServiceSVG} svg={ec2SVG} />
+                    </div>
+
+                    <div className="col col-xs-10">
+                      <h3>EC2</h3>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In aliquam malesuada magna, finibus sollicitudin metus porta ac.</p>
+                    </div>
+                  </div>
+                  </Padding>
+                </Padding>
               </div>
-
-              <p>Amazon EC2 Container Service (ECS) is a container management service
-              that lets you run applications from Docker containers on clusters of EC2 instances.</p>
-
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In aliquam malesuada magna, finibus sollicitudin metus porta ac.</p>
-
-
-              <Padding tb={2} r={4}>
-                <Padding tb={2}>
-                <div className="row">
-                  <div className="col col-xs-2 text-center">
-                    <BaseSVG className={style.awsServiceSVG} svg={ecsSVG} />
-                  </div>
-
-                  <div className="col col-xs-10">
-                    <h3>ECS</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In aliquam malesuada magna, finibus sollicitudin metus porta ac.</p>
-                  </div>
-                </div>
-                </Padding>
-
-                <Padding tb={2}>
-                 <div className="row">
-                  <div className="col col-xs-2 text-center">
-                    <BaseSVG className={style.awsServiceSVG} svg={rdsSVG} />
-                  </div>
-
-                  <div className="col col-xs-10">
-                    <h3>RDS</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In aliquam malesuada magna, finibus sollicitudin metus porta ac.</p>
-                  </div>
-                </div>
-                </Padding>
-
-                <Padding tb={2}>
-                 <div className="row">
-                  <div className="col col-xs-2 text-center">
-                    <BaseSVG className={style.awsServiceSVG} svg={ec2SVG} />
-                  </div>
-
-                  <div className="col col-xs-10">
-                    <h3>EC2</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In aliquam malesuada magna, finibus sollicitudin metus porta ac.</p>
-                  </div>
-                </div>
-                </Padding>
-              </Padding>
             </div>
           </div>
         </Panel>
@@ -98,22 +100,24 @@ export default React.createClass({
 
         <Panel>
           <div className="row">
-            <div className="col center-xs col-xs-12 col-sm-3 first-xs">
-              <Padding a={2}>
+            <div className="col center-xs col-xs-12 col-sm-4 col-md-3 first-xs">
+              <div className={style.logo}>
                 <BaseSVG svg={slackSVG} />
-              </Padding>
+              </div>
             </div>
 
-            <div className="col col-xs-12 col-sm-9 last-xs">
-              <div>
-                <h2 id="ec2">Rich Notifications with <span className="text-accent">Slack</span></h2>
-                <a href="#ec2"><small>(link icon)</small></a>
+            <div className="col col-xs-12 col-sm-8 col-md-9 last-xs">
+              <div className={style.prosePanel}>
+                <div className="center-xs">
+                  <h2 id="ec2">Rich Notifications with <span className="text-accent">Slack</span></h2>
+                  <a href="#ec2"><small>(link icon)</small></a>
+                </div>
+
+                <p>Amazon EC2 Container Service (ECS) is a container management service
+                that lets you run applications from Docker containers on clusters of EC2 instances.</p>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In aliquam malesuada magna, finibus sollicitudin metus porta ac.</p>
               </div>
-
-              <p>Amazon EC2 Container Service (ECS) is a container management service
-              that lets you run applications from Docker containers on clusters of EC2 instances.</p>
-
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In aliquam malesuada magna, finibus sollicitudin metus porta ac.</p>
             </div>
           </div>
         </Panel>
@@ -122,22 +126,24 @@ export default React.createClass({
 
         <Panel>
           <div className="row">
-            <div className="col center-xs col-xs-12 col-sm-3 first-xs">
-              <Padding a={2}>
+            <div className="col center-xs col-xs-12 col-sm-4 col-md-3 first-xs">
+              <div className={style.logo}>
                 <BaseSVG svg={pagerdutySVG} />
-              </Padding>
+              </div>
             </div>
 
-            <div className="col col-xs-12 col-sm-9 last-xs">
-              <div>
-                <h2 id="ec2">Incidence Response with <span className="text-accent">PagerDuty</span></h2>
-                <a href="#ec2"><small>(link icon)</small></a>
+            <div className="col col-xs-12 col-sm-8 col-md-9 last-xs">
+              <div className={style.prosePanel}>
+                <div className="center-xs">
+                  <h2 id="ec2">Incidence Response with <span className="text-accent">PagerDuty</span></h2>
+                  <a href="#ec2"><small>(link icon)</small></a>
+                </div>
+
+                <p>Amazon EC2 Container Service (ECS) is a container management service
+                that lets you run applications from Docker containers on clusters of EC2 instances.</p>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In aliquam malesuada magna, finibus sollicitudin metus porta ac.</p>
               </div>
-
-              <p>Amazon EC2 Container Service (ECS) is a container management service
-              that lets you run applications from Docker containers on clusters of EC2 instances.</p>
-
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In aliquam malesuada magna, finibus sollicitudin metus porta ac.</p>
             </div>
           </div>
         </Panel>
