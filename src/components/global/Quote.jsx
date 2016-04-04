59e1ec15a05d99cdad79405e8487fd3a6acdd473
Sara Bee <sara.bainbridge@gmail.com>
2016-04-04 14:20:05 -0400

    Add Azavea quote to try/pingdom

---
+++ src/components/global/Quote.jsx
@@ -0,0 +1,49 @@
+import cx from 'classnames';
+import React, { PropTypes } from 'react';
+import { Padding } from '../layout';
+import BaseSVG from '../images/BaseSVG';
+import style from './quote.css';
+
+export default React.createClass({
+  propTypes: {
+    // The quote itself
+    children: PropTypes.node.isRequired,
+    // The name of the person being quoted
+    author: PropTypes.string.isRequired,
+    // The quote author's current position at the given company
+    position: PropTypes.string,
+    // Optional. A company logo or similar
+    logo: PropTypes.string,
+    // Optional. A company URL
+    url: PropTypes.string,
+    className: PropTypes.string,
+  },
+
+  getClass() {
+    return cx(style.quote, this.props.className);
+  },
+
+  render() {
+    return (
+      <div className={this.getClass()}>
+        <Padding tb={1}>
+          <blockquote>{this.props.children}</blockquote>
+        </Padding>
+
+        <Padding tb={1}>
+          <div className={style.author}><strong>{this.props.author}</strong></div>
+          <div className={style.meta}><em>{this.props.position}</em></div>
+        </Padding>
+
+        { this.props.logo ?
+          <Padding tb={1}>
+            <a href={this.props.url} target="_blank">
+              <BaseSVG svg={this.props.logo} className={style.logo} />
+            </a>
+          </Padding>
+        : null }
+
+      </div>
+    );
+  }
+});
--- src/components/pages/try/TryPingdom.jsx
+++ src/components/pages/try/TryPingdom.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import style from './try.css';
-import { Padding } from '../../layout';
+import { Padding, Row, Col } from '../../layout';
+import SkewDivider from '../../layout/SkewDivider';
+import Panel from '../../panels/Panel';
 import { SplitContainer, SplitPanel } from '../../panels/SplitPanels';
-import BaseSVG from '../../images/BaseSVG';
-import cleanSimpleSVG from '../../images/clean-simple.svg';
+import Quote from '../../global/Quote';
 import TryCampaign from './TryCampaign';
+
+import BaseSVG from '../../images/BaseSVG';
+import azaveaLogo from '../../images/logos/azavea-logo.png';
+import cleanSimpleSVG from '../../images/clean-simple.svg';
 
 export default React.createClass({
   render() {
@@ -24,6 +29,18 @@ export default React.createClass({
             <BaseSVG svg={cleanSimpleSVG} className={style.svg} />
           </SplitPanel>
         </SplitContainer>
+
+        <SkewDivider className={style.offsetDivider} />
+
+        <Panel>
+          <Row>
+            <Col xs={10} xsOffset={1}>
+              <Quote author="Hector Castro" position="Operations Engineer" company="Azavea" url="http://www.azavea.com/" logo={azaveaLogo}>
+                <p>"Opsee is like Pingdom running inside of my AWS environment, but with more sophisticated options for where and how I apply health checks. I can see exactly what services and hosts are having problems. The notifications highlight issues in my responses, and I can resolve them right from the app."</p>
+              </Quote>
+            </Col>
+          </Row>
+        </Panel>
       </TryCampaign>
     );
   }
