bfbd7e5e97dd64d37915411c87777a51f44923e1
Sara Bee <sara.bainbridge@gmail.com>
2016-04-04 15:06:07 -0400

    Add quote to try/aws

--- src/components/pages/try/TryAWS.jsx
+++ src/components/pages/try/TryAWS.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import style from './try.css';
-import { Padding } from '../../layout';
+import { Padding, Row, Col } from '../../layout';
 import { SplitContainer, SplitPanel } from '../../panels/SplitPanels';
 import BaseSVG from '../../images/BaseSVG';
 import awsIntegrationsSVG from '../../images/integrations-aws.svg';
 import TryCampaign from './TryCampaign';
+import Panel from '../../panels/Panel';
+import SkewDivider from '../../layout/SkewDivider';
+import Quote from '../../global/Quote';
+import azaveaLogo from '../../images/logos/azavea-logo.png';
 
 export default React.createClass({
   render() {
@@ -24,6 +28,18 @@ export default React.createClass({
             <BaseSVG svg={awsIntegrationsSVG} className={style.svg} />
           </SplitPanel>
         </SplitContainer>
+
+        <SkewDivider />
+
+        <Panel>
+          <Row>
+            <Col xs={10} xsOffset={1}>
+              <Quote author="Hector Castro" position="Operations Engineer" company="Azavea" url="http://www.azavea.com/" logo={azaveaLogo}>
+                <p>"Opsee is like Pingdom running inside of my AWS environment, but with more sophisticated options for where and how I apply health checks. I can see exactly what services and hosts are having problems. The notifications highlight issues in my responses, and I can resolve them right from the app."</p>
+              </Quote>
+            </Col>
+          </Row>
+        </Panel>
       </TryCampaign>
     );
   }
