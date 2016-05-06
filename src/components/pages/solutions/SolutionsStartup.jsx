7e09da2598083cff35f6ad182891ad64d167252e
Stephen Boak <stephen.boak@gmail.com>
2016-05-06 13:27:18 -0700

    more solutions page stuff

--- src/components/pages/solutions/SolutionsStartup.jsx
+++ src/components/pages/solutions/SolutionsStartup.jsx
@@ -7,17 +7,22 @@ import { Padding, Row, Col } from '../../layout';
 import SkewDivider from '../../layout/SkewDivider';
 import SkewPanel from '../../panels/SkewPanel';
 import SignUpPanel from '../../panels/SignUpPanel';
-import Button from '../../forms/Button';
+// import Button from '../../forms/Button';
 // import Quote from '../../global/Quote';
 import Panel from '../../panels/Panel';
 
 import BaseSVG from '../../images/BaseSVG';
 // import checkDiagramSVG from '../../images/product-checks.svg';
-// import notificationsSVG from '../../images/product-notifications.svg';
-import awsIntegrationsSVG from '../../images/integrations-aws.svg';
+import notificationsSVG from '../../images/product-notifications.svg';
+// import awsIntegrationsSVG from '../../images/integrations-aws.svg';
 // import passingAssertionsSVG from '../../images/passing-assertions.svg';
 // import securitySVG from '../../images/security.svg';
-// import cleanSimpleSVG from '../../images/clean-simple.svg';
+import cleanSimpleSVG from '../../images/clean-simple.svg';
+import notificationLogosSVG from '../../images/notification-logos.svg';
+import howBastion from '../../images/how-bastion.svg';
+import checkEntities from '../../images/check-type-entities.svg';
+import checkCloudwatch from '../../images/check-type-cloudwatch.svg';
+import checkURL from '../../images/check-type-url.svg';
 
 const SolutionsStartup = React.createClass({
   render() {
@@ -34,10 +39,10 @@ const SolutionsStartup = React.createClass({
           <Panel>
             <Row>
               <Col xs={10} xsOffset={1}>
-                <p>The good news: a startup team running in AWS can grow to 20 or 30 engineers without dedicated ops</p>
-                <p>The bad news: now dev teams need to own their availability and performance</p>
+                <p><strong>The good news:</strong> a startup team running in AWS can grow to 20 or 30 engineers without dedicated ops</p>
+                <p><strong>The bad news:</strong> now dev teams need to own their availability and performance</p>
                 <p>Open-source tools are a pain to set up and configure, and today’s SaaS products are powerful but expensive, and overkill for our needs.</p>
-                <p>Opsee is here to help.</p>
+                <p><strong>Opsee is here to help.</strong></p>
               </Col>
             </Row>
           </Panel>
@@ -45,30 +50,85 @@ const SolutionsStartup = React.createClass({
           <SkewDivider />
 
           <SplitContainer className={style.section}>
-            <SplitPanel className={style.prosePanel}>
+            <SplitPanel className={style.screenshotProsePanel}>
               <Padding tb={1}>
-                <h2>Deep integration with <span className="text-accent">AWS</span></h2>
+                <h2><span className="text-accent">Health checks</span> have you covered</h2>
               </Padding>
-
+              <p>Continuously test your your services to be sure they’re working as expected</p>
               <ul>
-                <li><span className="prose">Just add our instance to your AWS environment</span></li>
-                <li><span className="prose">We automatically and continuously discover your environment</span></li>
-                <li><span className="prose">No configurations to manage</span></li>
+                <li><span className="prose">Failing health checks tell you exactly what’s wrong</span></li>
+                <li><span className="prose">Find problems without watching dashboards</span></li>
+                <li><span className="prose">Responsive UI works on any device</span></li>
               </ul>
-
-              <Padding tb={2}>
-                <Button to="/how" className={style.button} secondary chevron>
-                  How it works
-                </Button>
-              </Padding>
             </SplitPanel>
-
             <SplitPanel className={style.imagePanel}>
-              <BaseSVG svg={awsIntegrationsSVG} />
+              <BaseSVG svg={cleanSimpleSVG} className={style.screenshotSVG} />
+            </SplitPanel>
+          </SplitContainer>
+
+          <SkewDivider className={style.offsetDivider} />
+
+          <Padding b={1}>
+            <h2 className="text-center">Our health checks</h2>
+          </Padding>
+
+          <SplitContainer className={style.section}>
+            <SplitPanel className={style.imagePanelThirds}>
+              <Padding b={2}><h3 className="text-center">AWS Resources</h3></Padding>
+              <BaseSVG svg={checkEntities} className={style.screenshotSVG} />
+              <p>Our checks auto scale with your environment, tracking membership in Security Groups, Auto Scale Groups, and ELBs automatically. Verify the headers, JSON keys, and round-trip times of our responses too</p>
+            </SplitPanel>
+            <SplitPanel className={style.imagePanelThirds}>
+              <Padding b={2}><h3 className="text-center">CloudWatch Metrics</h3></Padding>
+              <BaseSVG svg={checkCloudwatch} className={style.screenshotSVG} />
+              <p>Set thresholds on all of your important CloudWatch metrics.</p>
+            </SplitPanel>
+            <SplitPanel className={style.imagePanelThirds}>
+              <Padding b={2}><h3 className="text-center">URLs</h3></Padding>
+              <BaseSVG svg={checkURL} className={style.screenshotSVG} />
+              <p>If you have external dependencies, websites, and CDNs that need coverage, Opsee will track all DNS entries for a URL automatically and notify you of failures.</p>
             </SplitPanel>
           </SplitContainer>
 
           <SkewDivider />
+
+          <SplitContainer className={style.section}>
+            <SplitPanel className={style.imagePanel}>
+              <BaseSVG svg={notificationsSVG} className={style.screenshotSVG} />
+            </SplitPanel>
+            <SplitPanel className={style.screenshotProsePanel}>
+              <Padding tb={1}>
+                <h2>Rich, actionable <span className="text-accent">notifications</span></h2>
+              </Padding>
+              <ul>
+                <li><span className="prose">See failing responses in context</span></li>
+                <li><span className="prose">Get notified anywhere: email, Slack, webhooks, or PagerDuty</span></li>
+                <li><span className="prose">Take action on problem instances (restart, stop, or terminate)</span></li>
+              </ul>
+              <Padding t={3}>
+                <BaseSVG svg={notificationLogosSVG} className={style.screenshotSVG} />
+              </Padding>
+            </SplitPanel>
+          </SplitContainer>
+
+          <SkewDivider className={style.offsetDivider} />
+
+          <SplitContainer className={style.section}>
+            <SplitPanel className={style.prosePanel}>
+              <Padding b={1}>
+                <h2>Just add <span className="text-accent">our instance</span></h2>
+              </Padding>
+              <ul>
+                <li><span className="prose">Our instance is t2.micro and is <a target="_blank" href="https://aws.amazon.com/ec2/pricing/">free-tier eligible</a></span></li>
+                <li><span className="prose">It uses AWS APIs to discover your environment, detecting changes to infrastructure automatically</span></li>
+                <li><span className="prose">Permissions are managed by our cross-account role</span></li>
+              </ul>
+            </SplitPanel>
+            <SplitPanel className={style.imagePanel}>
+              <BaseSVG svg={howBastion} className={style.svg} />
+            </SplitPanel>
+          </SplitContainer>
+
         </div>
 
         <SkewPanel backgroundColor="#333" skewBottom={false}>
