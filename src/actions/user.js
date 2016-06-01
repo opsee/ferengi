0c9a19d0e1645961089b9530a6a87b7a0728915a
Sara Bee <sara.bainbridge@gmail.com>
2016-06-01 17:23:29 -0400

    :bath: README

--- src/actions/user.js
+++ src/actions/user.js
@@ -18,30 +18,30 @@ function setUser(userData) {
 }
 
 function doSignup(data = {}) {
-  // return Promise.resolve({"intercom_hmac":"bf471da9f3e5ce6d291c49a1e51e8b8de570498e1f7227bf48b732fa1c490967","token":"eyJhbGciOiJBMTI4R0NNS1ciLCJlbmMiOiJBMTI4R0NNIiwiaXYiOiJzejRlUGhhTnRCVk1PRzdVIiwidGFnIjoiS3FHamdfUVkybm1lRzFENUZTczUydyJ9.6t3R7w531I0jTthkSUXkSw.tb5-D2R4kiR8BVjL.Jcatcn7R3SWQFMuajmExPADMbg2JBWN-921Mf-WUWEjOvIOd2WIk_Rifn1BJzOzpR3fb1Jnz3DfTLnHZzZSx7qTHeIdpYOKMvG1i2pOicmskO-5HRraDRGe6Jh3rFA4MhYZaVn7vkFixTyhLLC0KZdy-M-KFnZbNejMa5dFfD4kJXvKwjiUh_10pG9KSf58tsXOONCeV3USrNRoGRcxrd62sgaAGXR1SD4LgdqD6vH9LwU5bahSppTU2sbHMY3S621EOIkjxXSHMnhoKkuDZzQf9U9vWFBcQaQZ-SYc0E1PMqtgOsoRSPy44KvtcZAArNWAwPjs9CXBBnfNYQIM6agYDrK0d9v9QoiI.WQor5ei2-D28zbWwRSJdQg","user":{"id":310,"customer_id":"4a9dad90-281a-11e6-ba24-2fbabd57b5f6","email":"sara+local11@opsee.co","name":"sara+local11@opsee.co","active":true,"created_at":1464800406747,"updated_at":1464800406747,"status":"active","perms":[]}});
-  return new Promise((resolve, reject) => {
-    fetch('https://auth.opsee.com/signups/activate', {
-      method: 'post',
-      body: JSON.stringify(_.pick(data, ['email', 'referrer', 'name']))
-    })
-    .then(res => {
-      if (!res.ok){
-        return res.json().then(json => {
-          throw new Error(json.message);
-        }).catch(reject);
-      }
+  return Promise.resolve({"intercom_hmac":"bf471da9f3e5ce6d291c49a1e51e8b8de570498e1f7227bf48b732fa1c490967","token":"eyJhbGciOiJBMTI4R0NNS1ciLCJlbmMiOiJBMTI4R0NNIiwiaXYiOiJzejRlUGhhTnRCVk1PRzdVIiwidGFnIjoiS3FHamdfUVkybm1lRzFENUZTczUydyJ9.6t3R7w531I0jTthkSUXkSw.tb5-D2R4kiR8BVjL.Jcatcn7R3SWQFMuajmExPADMbg2JBWN-921Mf-WUWEjOvIOd2WIk_Rifn1BJzOzpR3fb1Jnz3DfTLnHZzZSx7qTHeIdpYOKMvG1i2pOicmskO-5HRraDRGe6Jh3rFA4MhYZaVn7vkFixTyhLLC0KZdy-M-KFnZbNejMa5dFfD4kJXvKwjiUh_10pG9KSf58tsXOONCeV3USrNRoGRcxrd62sgaAGXR1SD4LgdqD6vH9LwU5bahSppTU2sbHMY3S621EOIkjxXSHMnhoKkuDZzQf9U9vWFBcQaQZ-SYc0E1PMqtgOsoRSPy44KvtcZAArNWAwPjs9CXBBnfNYQIM6agYDrK0d9v9QoiI.WQor5ei2-D28zbWwRSJdQg","user":{"id":310,"customer_id":"4a9dad90-281a-11e6-ba24-2fbabd57b5f6","email":"sara+local11@opsee.co","name":"sara+local11@opsee.co","active":true,"created_at":1464800406747,"updated_at":1464800406747,"status":"active","perms":[]}});
+  // return new Promise((resolve, reject) => {
+  //   fetch('https://auth.opsee.com/signups/activate', {
+  //     method: 'post',
+  //     body: JSON.stringify(_.pick(data, ['email', 'referrer', 'name']))
+  //   })
+  //   .then(res => {
+  //     if (!res.ok){
+  //       return res.json().then(json => {
+  //         throw new Error(json.message);
+  //       }).catch(reject);
+  //     }
 
-      // Track successful signups only
-      trackEvent(ONBOARD, ONBOARD_SIGNUP, data);
+  //     // Track successful signups only
+  //     trackEvent(ONBOARD, ONBOARD_SIGNUP, data);
 
-      return res.json();
-    })
-    .then(resolve)
-    .catch(err => {
-      yeller.report(err);
-      reject(err);
-    });
-  });
+  //     return res.json();
+  //   })
+  //   .then(resolve)
+  //   .catch(err => {
+  //     yeller.report(err);
+  //     reject(err);
+  //   });
+  // });
 }
 
 function makeCheck(userData, data) {
