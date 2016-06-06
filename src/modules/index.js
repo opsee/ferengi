44b249de84f9412d3993e89ec29012f7955d30a3
Sara Bee <sara.bainbridge@gmail.com>
2016-06-06 16:23:09 -0400

    Renaming

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,9 +1,13 @@
 import ga from './ga';
 import referrer from './referrer';
+import renderClient from './renderClient';
+import renderServer from './renderServer';
 import yeller from './yeller';
 
 export {
   ga,
   referrer,
+  renderClient,
+  renderServer,
   yeller
 };
