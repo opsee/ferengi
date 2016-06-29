97f42d6b3bbfb1eb9d93895d0d5a580019229345
Sara Bee <sara.bainbridge@gmail.com>
2016-06-29 10:50:49 -0400

    Pricing page tweaks

--- src/components/pages/Pricing.jsx
+++ src/components/pages/Pricing.jsx
@@ -1,23 +1,20 @@
+import cx from 'classnames';
 import React from 'react';
+
 import StaticHeader from '../panels/StaticHeader';
 import Panel from '../panels/Panel';
 import SkewPanel from '../panels/SkewPanel';
 import SignUpPanel from '../panels/SignUpPanel';
 import SkewDivider from '../layout/SkewDivider';
-import { Padding, Row, Col } from '../layout';
-// import { Heading, Permalink } from '../type';
+import { Padding, Grid, Row, Col } from '../layout';
+import { Button } from '../forms';
+import { Quote } from '../global';
+
+import style from './pricing.css';
 import BaseSVG from '../images/BaseSVG';
 import infinitySVG from '../images/infinity.svg';
 import latencySVG from '../images/latency.svg';
 import notificationLogosWhiteSVG from '../images/notification-logos-white.svg';
-// import awsSVG from '../images/logos/aws-01.svg';
-// import slackSVG from '../images/logos/slack-01.svg';
-// import pagerdutySVG from '../images/logos/pagerduty.svg';
-// import ec2SVG from '../images/logos/ec2-01.svg';
-// import ecsSVG from '../images/logos/ecs-01.svg';
-// import rdsSVG from '../images/logos/rds-01.svg';
-
-import style from './pricing.css';
 
 export default React.createClass({
   render() {
@@ -26,7 +23,7 @@ export default React.createClass({
         <StaticHeader>
           <div className="text-center">
             <h1>Only pay for <span className="text-accent-static">what you use</span></h1>
-            <h3>Coming soon. We&rsquo;re still in beta, but here is what you can expect</h3>
+            <h3>Coming soon. We&rsquo;re still in beta, but here is what you can expect.</h3>
           </div>
         </StaticHeader>
 
@@ -36,46 +33,53 @@ export default React.createClass({
             <p>Try Opsee for free, for as long as you want, with up to 5 health checks on public URLs or APIs.</p>
           </Padding>
 
-          <Row>
-            <Col xs={12} sm={6}>
-              <h3 className={style.pricingTableTHFree}>Free</h3>
-              <div className={style.priceFree}>
-                <span className={style.priceDol}>$</span><span className={style.priceNum}>0</span>
-                <Padding t={1}>
-                  <div className={style.priceNote}>free<br/>forever</div>
-                </Padding>
-              </div>
+          <Grid fluid>
+            <Row>
+              <Col xs={12} sm={6}>
+                <div className={style.tier}>
+                  <div className={style.pricing}>
+                    <h3>Free</h3>
+                    <Padding tb={1}>
+                      <div className={style.price}>$0</div>
+                      forever
+                    </Padding>
+                  </div>
+                  <ul className={style.features}>
+                    <li>Up to 5 global checks</li>
+                    <li>5-minute check frequency</li>
+                    <li>Checks run from 1 of our global locations</li>
+                    <li className={style.featureDisabled}>AWS coverage with our EC2 Instance</li>
+                    <li className={style.featureDisabled}>CloudWatch integration with unlimited metrics per check</li>
+                  </ul>
+                  <div className="text-center">
+                    <Button>Sign Up</Button>
+                  </div>
+                </div>
+              </Col>
 
-              <Padding lr={1} b={2}>
-                <h3>Features</h3>
-                <ul className="prose">
-                  <li>Up to 5 Global Checks</li>
-                  <li>5-minute check frequency</li>
-                  <li>Checks run from 1 of our global locations</li>
-                </ul>
-              </Padding>
-            </Col>
-            <Col xs={12} sm={6}>
-              <h3 className={style.pricingTableTH}>AWS Checks</h3>
-
-              <div className={style.price}>
-                <span className={style.priceDol}>$</span><span className={style.priceNum}>5</span>
-                <Padding t={1}>
-                  <div className={style.priceNote}>per check<br/>per month</div>
-                </Padding>
-              </div>
-
-              <Padding lr={1} b={2}>
-                <h3>Features</h3>
-                <ul className="prose">
-                  <li>30-second check frequency</li>
-                  <li>AWS coverage with <a href="/how">our EC2 Instance</a></li>
-                  <li>Unlimited hosts</li>
-                  <li>CloudWatch integration with unlimited metrics per check</li>
-                </ul>
-              </Padding>
-            </Col>
-          </Row>
+              <Col xs={12} sm={6}>
+                <div className={style.tier}>
+                  <div className={style.pricing}>
+                    <h3>Developer</h3>
+                    <Padding tb={1}>
+                      <div className={style.price}>$5</div>
+                      per check, per month
+                    </Padding>
+                  </div>
+                  <ul className={style.features}>
+                    <li>Unlimited global checks</li>
+                    <li>30-second check frequency</li>
+                    <li>Unlimited hosts</li>
+                    <li>AWS coverage with <a href="/how">our EC2 Instance</a></li>
+                    <li>CloudWatch integration with unlimited metrics per check</li>
+                  </ul>
+                  <div className="text-center">
+                    <Button>Sign Up</Button>
+                  </div>
+                </div>
+              </Col>
+            </Row>
+          </Grid>
         </Panel>
 
         <SkewDivider />
