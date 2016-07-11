e78eb615556b77c3048c581c93dd6f89e8265c26
Corey Light <coreylight@gmail.com>
2016-07-11 13:51:16 -0500

    use location if no referrer is present

--- src/modules/referrer.js
+++ src/modules/referrer.js
@@ -3,12 +3,16 @@ import * as storage from './storage';
 
 export function getReferrer(location = {}) {
   let referrer = _.get(location, 'query.referrer');
-  return referrer || storage.getItem('referrer');
+  let fallback = '';
+  if (typeof window !== 'undefined'){
+    fallback = document.referrer || `(none) ${window.location.href}`;
+  }
+  return referrer || storage.getItem('referrer') || fallback;
 }
 
 export function setReferrer(location) {
   if (typeof window !== 'undefined'){
-    const referrer = getReferrer(location) || document.referrer;
+    const referrer = getReferrer(location);
     storage.setItem('referrer', referrer);
   }
 }
