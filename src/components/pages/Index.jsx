642119adb52754f47c6eff7a8896519bfcd038cf
Stephen Boak <stephen.boak@gmail.com>
2016-07-11 13:53:54 -0700

    banner

--- src/components/pages/Index.jsx
+++ src/components/pages/Index.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
+import style from './index.css';
 
+import { getReferrer } from '../../modules/referrer';
 import { Quote } from '../global';
 import { Row, Col } from '../layout';
 import SkewDivider from '../layout/SkewDivider';
 import Header from '../Header';
+import Padding from '../layout/Padding';
 
 import FeaturedCustomers from '../panels/FeaturedCustomers';
 import HeroPanel from '../panels/HeroPanel';
@@ -17,10 +20,29 @@ import styleConstants from '../../constants/styleConstants';
 /*eslint-enable no-unused-vars*/
 
 export default React.createClass({
+  propTypes: {
+    location: PropTypes.object
+  },
+  getReferrer(){
+    return getReferrer(this.props.location);
+  },
+  renderTitle(){
+    if (this.getReferrer() === 'producthunt') {
+      return (
+        <Padding className={style.referralbanner} tb={1} lr={2}>
+          <h2>Welcome Product Hunt!</h2>
+          <h3>Sign up today and get 2 extra weeks of free Opsee</h3>
+        </Padding>
+      );
+    }
+    return null;
+  },
   render() {
     return (
       <div>
         <Header theme="dark"/>
+
+        {this.renderTitle()}
 
         <HeroPanel />
         <SkewDivider />
--- src/components/pages/Summit.jsx
+++ src/components/pages/Summit.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
+import style from './index.css';
 
+import { getReferrer } from '../../modules/referrer';
 import { Quote } from '../global';
 import { Row, Col } from '../layout';
 import SkewDivider from '../layout/SkewDivider';
 import Header from '../Header';
+import Padding from '../layout/Padding';
 
 import FeaturedCustomers from '../panels/FeaturedCustomers';
 import HeroPanel from '../panels/HeroPanel';
@@ -17,15 +20,30 @@ import styleConstants from '../../constants/styleConstants';
 /*eslint-enable no-unused-vars*/
 
 export default React.createClass({
+  propTypes: {
+    location: PropTypes.object
+  },
+  getReferrer(){
+    return getReferrer(this.props.location);
+  },
+  renderTitle(){
+    if (this.getReferrer().match('/summit')) {
+      return (
+        <Padding className={style.referralbanner} tb={1} lr={2}>
+          <h2>Welcome AWS Summit!</h2>
+          <h3>Sign up from this page and get 2 extra weeks of free Opsee</h3>
+        </Padding>
+      );
+    }
+
+    return null;
+  },
   render() {
     return (
       <div>
         <Header theme="dark"/>
 
-        <Panel>
-          <h2 className="text-center">Welcome AWS Summit!</h2>
-          <h3 className="text-center">Sign up from this page and get 2 extra weeks of free Opsee</h3>
-        </Panel>
+        {this.renderTitle()}
 
         <HeroPanel />
         <SkewDivider />
