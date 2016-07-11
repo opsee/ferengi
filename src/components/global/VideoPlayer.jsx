379bbc1f3c42205651882c71b01639044b4c6f3f
Stephen Boak <stephen.boak@gmail.com>
2016-07-11 15:37:28 -0700

    icon

--- src/components/global/VideoPlayer.jsx
+++ src/components/global/VideoPlayer.jsx
@@ -2,7 +2,7 @@ import cx from 'classnames';
 import React, { PropTypes } from 'react';
 import {Button} from '../forms';
 import style from './videoPlayer.css';
-import {Play} from 'emissary/src/js/components/icons';
+import {PlayCircled} from 'emissary/src/js/components/icons';
 
 const VideoPlayer = React.createClass({
   propTypes: {
@@ -26,7 +26,7 @@ const VideoPlayer = React.createClass({
   render() {
     return (
       <div className={style.outer}>
-        {!this.state.show && <Button secondary="true" onClick={this.handleButtonClick} className={style.button}>Watch the video&nbsp;<Play btn/></Button>}
+        {!this.state.show && <Button secondary="true" onClick={this.handleButtonClick} className={style.button}>Watch the video&nbsp;<PlayCircled btn/></Button>}
         <div className={cx(style.wrapper, this.state.show && style.show)}>
           <iframe src="https://player.vimeo.com/video/173789397?color=e53a35&title=0&byline=0&portrait=0" className={style.iframe} frameBorder="0" allowFullScreen id="video"></iframe>
         </div>
