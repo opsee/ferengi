6a37ca74a70891dbf97b22248bf8a4f1e4534e93
Stephen Boak <stephen.boak@gmail.com>
2016-07-11 17:00:00 -0700

    i cant believe this fucking footer link was wrong for this long

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -32,7 +32,7 @@ export default React.createClass({
         <ul className={style.nav}>
           <li><Link to="/guides/nodechecks">node.js</Link></li>
           <li><Link to="/guides/gochecks">Go</Link></li>
-          <li><Link to="/guides/cloudwatch">CloudWatch</Link></li>
+          <li><Link to="/guides/awsmonitoring">AWS Monitoring</Link></li>
           <li><Link to="/guides/dropwizard">Dropwizard</Link></li>
           <li><Link to="https://app.opsee.com/help" target="_blank">Documentation</Link></li>
         </ul>
